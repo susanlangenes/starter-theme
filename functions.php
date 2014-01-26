263ef2b49e31f916d911f5ed568f8fd0710ea7ed
Iconoblast <salvatore.the.candela@gmail.com>
2014-01-25 19:10:47 -0800

    Added 3-column sidebar

--- functions.php
+++ functions.php
@@ -100,6 +100,14 @@ function starter_theme_widgets_init() {
 		'before_title'  => '<h3 class="widget-title">',
 		'after_title'   => '</h3><div class="inner-widget-area">',
 	) );
+	  register_sidebar( array(
+    'name'          => __( 'Home 3-Column', 'starter-theme' ),
+    'id'            => 'sidebar-3',
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</aside>',
+    'before_title'  => '<h1 class="widget-title">',
+    'after_title'   => '</h1>',
+  ) );
 }
 add_action( 'widgets_init', 'starter_theme_widgets_init' );
 
